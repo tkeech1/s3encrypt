c0f12cdbe5fc5a273004ec24241ac235366cae48
Todd Keech <tkeech1@hotmail.com>
2020-08-22 08:43:09 -0400

    dockerfile tweaks to get tests running in vscode

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,50 +5,6 @@ import pytest
 from s3encrypt.__main__ import main
 from s3encrypt.s3encrypt import S3EncryptError
 
-"""@mock.patch("s3encrypt.__main__.sys")
-def test_get_args(mock_sys: mock.Mock) -> None:
-    mock_sys.argv = [
-        "cmd",
-        "--directories",
-        "/",
-        "--s3_bucket",
-        "test",
-        "--password",
-        "pass",
-    ]
-    get_args()
-
-    mock_sys.argv = [
-        "cmd",
-        "--directories",
-        "/",
-        "--s3_bucket",
-        "test",
-    ]
-    with pytest.raises(SystemExit):
-        get_args()
-
-    mock_sys.argv = [
-        "cmd",
-        "--directories",
-        "/",
-        "--password",
-        "pass",
-    ]
-    with pytest.raises(SystemExit):
-        get_args()
-
-    mock_sys.argv = [
-        "cmd",
-        "--s3_bucket",
-        "test",
-        "--password",
-        "pass",
-    ]
-    with pytest.raises(SystemExit):
-        get_args()
-"""
-
 
 @mock.patch("s3encrypt.__main__.DirectoryWatcher")
 @mock.patch("s3encrypt.__main__.sys")
